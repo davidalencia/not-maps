eaab6eb3ef1698b578a2fcc927bdd2df68dbf2cb
David Valencia <davidalencia@gmail.com>
2024-09-01 20:22:01 -0600

    proof of concept

---
+++ index.js
@@ -0,0 +1,29 @@
+const { Client } = require("@notionhq/client");
+require("dotenv").config();
+
+const notion = new Client({
+  auth: process.env.NOTION_API_KEY,
+});
+
+const express = require("express");
+var cors = require("cors");
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get("/locations/:id", async (req, res) => {
+  const databaseId = req.params.id;
+  if (!databaseId) return res.status(400).send("No id provided");
+  const response = await notion.databases.query({ database_id: databaseId });
+  const locations = response.results.map((x) => ({
+    lat: x.properties.Lat.number,
+    lon: x.properties.Lon.number,
+    name: x.properties.Name.title.reduce((pv, cv) => pv + cv.plain_text, ""),
+  }));
+  res.json(locations);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
